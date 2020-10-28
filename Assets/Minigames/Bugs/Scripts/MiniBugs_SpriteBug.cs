62658e27f12ede87c10e02aeb892fe5acbed0767
James <jamesmoulang123@gmail.com>
2020-10-28 01:25:35 +0000

    ants and grass

--- Assets/Minigames/Bugs/Scripts/MiniBugs_SpriteBug.cs
+++ Assets/Minigames/Bugs/Scripts/MiniBugs_SpriteBug.cs
@@ -8,8 +8,10 @@ public class MiniBugs_SpriteBug : MonoBehaviour {
     public bool UseShadowTarget = false;
     public GameObject[] Bugs;
     public float Altitude = 0f;
+    public bool BlobShadow = true;
 
     private List<Vector3> bugAnchorPoints = new List<Vector3>();
+    private Vector3 nonBlobShadowOffset;
 
     void Start() {
         foreach (GameObject bug in Bugs) {
@@ -19,6 +21,15 @@ public class MiniBugs_SpriteBug : MonoBehaviour {
                 bug.transform.localPosition.z
             ));
         }
+
+        if (!BlobShadow) {
+            nonBlobShadowOffset = new Vector3(
+                Shadow.transform.localPosition.x,
+                Shadow.transform.localPosition.y,
+                Shadow.transform.localPosition.z
+            );
+            Shadow.transform.parent = null;
+        }
     }
 
     void Update() {
@@ -27,12 +38,25 @@ public class MiniBugs_SpriteBug : MonoBehaviour {
             bug.transform.localPosition = bugAnchorPoints[i] + Vector3.up * Altitude;
         }
 
+        if (!BlobShadow) {
+            Shadow.transform.rotation = transform.rotation;
+            Shadow.transform.position = transform.position + nonBlobShadowOffset;
+        }
+
         if (UseShadowTarget) {
-            Shadow.transform.localPosition = new Vector3(
-                Shadow.transform.localPosition.x,
-                Shadow.transform.localPosition.y,
-                ShadowZTarget - transform.position.z
-            );
+            if (BlobShadow) {
+                Shadow.transform.localPosition = new Vector3(
+                    Shadow.transform.localPosition.x,
+                    Shadow.transform.localPosition.y,
+                    ShadowZTarget - transform.position.z
+                );
+            } else {
+                Shadow.transform.position = new Vector3(
+                    Shadow.transform.position.x,
+                    Shadow.transform.position.y,
+                    ShadowZTarget
+                );
+            }
         }
     }
 }
---
+++ Assets/Minigames/Bugs/Scripts/MiniBugs_SpriteOrder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBugs_SpriteOrder : MonoBehaviour {
+    public float CloseZ = 5f;
+    public float FarZ = 7f;
+
+    private float w;
+    private float h;
+    private Camera mainCamera;
+
+    void Start() {
+        mainCamera = Camera.main;
+        w = mainCamera.orthographicSize * Screen.width / Screen.height;
+        h = mainCamera.orthographicSize;
+    }
+
+    void Update() {
+        Vector3 pos = transform.position;
+        pos.z = Mathf.Lerp(CloseZ, FarZ, ((pos.y - mainCamera.transform.position.y) + h) / (2f * h));
+        transform.position = pos;
+    }
+}
